ec92423a5d87016ae0d024d38a918322d7150258
Samuel Easton (RIT Student) <sge3418@rit.edu>
2026-04-03 17:37:25 -0400

    the state machine grows...

--- Scenes/Customer.cs
+++ Scenes/Customer.cs
@@ -4,16 +4,40 @@ using System;
 
 public partial class Customer : CharacterBody2D
 {
-	//===GAMBLING CONTROLS===
+	//======BEHAVIOUR CONTROLS======
+	public CustomerGoal CurrentGoal = CustomerGoal.WANDER;
+	private float _wanderTime = 5f; //number of seconds to wander
+
+	//How "Hopeful"/satisfied this character is - factors into decision making. ranges 0-1
+	private float _hopeAmount = 1;
+	public float HopeAmount { get { return _hopeAmount; } set { _hopeAmount = Mathf.Clamp(value, 0, 1); } }
+
+	//how addicted to gambling this character is. The more addicted to gambling someone is, the less rationally they act!
+	private float _addictionStrength = 0.05f;
+	public float AddictionStrength { get { return _addictionStrength; } set { _addictionStrength += Mathf.Clamp(value, 0, 1); } }
+
+    //Hope controls represent an offset/scalar (depending) applied to _hopeAmount when used in calculations
+    [ExportGroup("Hope Controls")]
+	[Export] public float HopeWinRateStrength = .1f; //how much hope shifts percieved win rate when deciding if we can make a profit
+	[Export] public float HopeBorrowWillingness = .75f; //the maximum chance the customer is willing to put themselves in debt (when hope = 1)
+	[Export] public float HopeLeaveRateStrength = .3f;
+
+    [ExportGroup("")]
+	
+
+
+	//======GAMBLING CONTROLS======
 	public float CurrentMoney = 100; //100 temp value
 	public float StartingMoney = 100; //100 temp value
+	//represents how much this character has "made" - 1 is no profit but no loss, 0-1 is some amount of loss but no debt, >1 is profiting, <0 is in debt 
+	public float CurrentEarningPercent { get { return CurrentMoney / StartingMoney; } }
 	private Dictionary<Machine, Array<bool>> _machineWinRates = new Dictionary<Machine, Array<bool>>();
 
 	public Machine ActiveMachine;
 	private const int MIN_PLAYS_TO_GUESS_RATE = 5;
 
 
-    //===NAV CONTROLS===
+    //======NAV CONTROLS======
     private NavigationAgent2D _navAgent;
     [Export] public float Speed = 300.0f;
 	private float _movementDelta;
@@ -25,6 +49,9 @@ public partial class Customer : CharacterBody2D
 		get { return _navAgent.TargetPosition; }
 		set { _navAgent.TargetPosition = value;}
 	}
+
+	//======EXTRA VARS======
+	private RandomNumberGenerator _rng;
 
     public override void _Ready()
     {
@@ -38,7 +65,7 @@ public partial class Customer : CharacterBody2D
 
 		_navAgent.VelocityComputed += OnVelocityComputed;
 
-
+		_rng = new RandomNumberGenerator();
 
 
     }
@@ -70,6 +97,77 @@ public partial class Customer : CharacterBody2D
 		}
 
 	}
+
+	//checks current states and sets CurrentGoal to the appropriate Goal
+	public void ReevaluateGoal()
+	{
+
+		switch(CurrentGoal)
+		{
+			case CustomerGoal.GAMBLE:
+                //if we are gambling and think we can stand to make a profit at this machine, KEEP GOING BABY MORE GAMBLING!! (maybe)
+                if (GetPercievedMachineProfit(ActiveMachine) > 0)
+                {
+                    //check if using this machine even once will put us into debt
+                    float debtChance = (1 - GetPercievedWinRate(ActiveMachine)) * (_hopeAmount * HopeBorrowWillingness);
+                    if ((ActiveMachine.Cost >= CurrentMoney && _rng.Randf() >= Mathf.Min(debtChance, 1 - _addictionStrength)) || ActiveMachine.Cost < CurrentMoney)
+                    {
+                        //it either WON'T put us into debt OR we think that it's an acceptable risk (rolled lower than debtChance or we're addicted)
+                        CurrentGoal = CustomerGoal.GAMBLE;
+                        return;
+                    }
+
+					//decided it was too risky, fall through to wander
+					break;
+
+                }
+				else
+				{
+
+                    //if we're gambling but we don't think we can make a profit, maybe we're wrong?
+					if(_rng.Randf() < _addictionStrength)
+					{
+						CurrentGoal = CustomerGoal.GAMBLE;
+						return;
+					}
+                }
+
+
+                break;
+
+			case CustomerGoal.WANDER:
+				//if we're wandering, we either just entered or we left a machine for some reason. We need to analyze what we want to do.
+
+				//NOTE: fleeing from "max debt" is NOT included in this logic, when we lose money so that CurrentEarningPercent is <= -1, the character should ALWAYS flee, otherwise addicts might never leave
+				//case "0" -> gambling addicition. No matter what, if we're addicted to gambling, there's a chance we gamble again
+				if (_rng.Randi() < _addictionStrength)
+				{
+					CurrentGoal = CustomerGoal.GAMBLE;
+					return;
+				}
+
+				//case 1: we've made profit
+					//consider leaving? if our hope is low, we should leave, otherwise we should stay
+
+				//case 2: we're at our break even
+					//again consider leaving, but if our hope is low we should REALLY consider leaving. With high hope we should REALLY consider staying
+
+				//case 3: we're at a loss
+					//again consider leaving, but if our hope is low we should ABSOLUTELY consider leaving. with high hope we should ABSOLUTELY stay!! we can make it back!!
+
+				//case 4: we're in debt
+					//oh shit. oh fuck. either we can make it back or we need to FLEE.
+
+				break;
+		}
+
+
+		//safety check, we didn't reach any real conclusion, in which case we should just wander...
+		CurrentGoal = CustomerGoal.WANDER;
+		GetTree().CreateTimer(_wanderTime).Timeout += ReevaluateGoal;
+	}
+
+
 
 	public void OnVelocityComputed(Vector2 safeVelocity)
 	{
@@ -126,4 +224,25 @@ public partial class Customer : CharacterBody2D
 		return percievedWins / potentialMaxWins;
 	}
 
+	/// <summary>
+	/// given a machine, returns how much this Customer thinks that machine will make them
+	/// </summary>
+	/// <param name="m"></param>
+	/// <returns></returns>
+	public float GetPercievedMachineProfit(Machine m)
+	{
+		float rate = GetPercievedWinRate(m);
+
+		return (m.Profit * Mathf.Clamp(rate +  _hopeAmount * HopeWinRateStrength, 0, 1)) - (m.Cost * (1 - rate));
+
+	}
+
+}
+
+public enum CustomerGoal
+{
+	GAMBLE, //find an active machine and use it
+	WANDER, //walk around for a little
+	LEAVE,  //Leave normally, without potentially dying (not in debt)
+	FLEE    //get the FUCK out because you're in debt and don't think you can make it back
 }
--- Scenes/Machine.cs
+++ Scenes/Machine.cs
@@ -5,6 +5,9 @@ public partial class Machine : Node2D
 {
 	//hii juliaaaa I just made this so "Machines" can exist and be "open" that's literally all I need it for kthxby -Sam
 	public bool IsAvailable = true;
+	public float Cost = 1;
+	public float Profit = 10;
+
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
