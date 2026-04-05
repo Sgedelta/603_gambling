e7961e710d725d4c4171ecf44ddd268e75121f98
JRissberger <JRissberger@gmail.com>
2026-04-04 22:46:54 -0400

    gambling works yay

--- Scenes/Machine.cs
+++ Scenes/Machine.cs
@@ -8,17 +8,22 @@ public partial class Machine : Node2D
 	public Node2D PlayPosition;
 	[Export] public float Cost = 1;
 	[Export] public float Profit = 10;
+	[Export] public float WinChance = 0.1f; //Should be between 0 and 1, can adjust as needed
 
 	[Export] public float PlayTime;
 
 	[Export] public float PlayDistance = 100;
 	[Signal] public delegate void OnGamePlayedEventHandler(bool won); //signal for when the game is over
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    private RandomNumberGenerator rng;
+    
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
 		PlayPosition = GetNode<Node2D>("PlayPosition");
-	}
+        rng = new RandomNumberGenerator();
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -33,8 +38,22 @@ public partial class Machine : Node2D
 		await ToSignal(GetTree().CreateTimer(PlayTime), SceneTreeTimer.SignalName.Timeout);
 
 		//decide if we win
+		bool win = rng.Randf() < WinChance;
 
-		EmitSignal(SignalName.OnGamePlayed, true); //TEMP TRUE MEANS ALWAYS WIN!! this casino sucks ASS 
+		//Adjust customer's money and casino's money
+		//customer has to spend to even play, always reduce their money by cost
+		c.CurrentMoney -= Cost;
+
+		//Add money
+		if (win)
+		{
+			c.CurrentMoney += Profit;
+		}
+
+		GD.Print(c.CurrentMoney);
+
+		//Casino no longer sucks ass, returns result to player
+		EmitSignal(SignalName.OnGamePlayed, win); 
 
 	}
 }
