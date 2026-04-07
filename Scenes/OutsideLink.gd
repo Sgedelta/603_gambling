0e4f78f7d7e685cb4592d9eb0b03a026725d01ce
Gia Hur <129004059+giahur@users.noreply.github.com>
2026-04-06 21:01:30 -0400

    switch to canvaslayer to help with visibility

--- Scenes/OutsideLink.gd
+++ Scenes/OutsideLink.gd
@@ -3,6 +3,5 @@ extends Button
 func _on_pressed() -> void:
 	var payment = load("res://Scenes/AD_Microtransaction.tscn").instantiate()
 	payment.process_mode = Node.PROCESS_MODE_ALWAYS
-	payment.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
 	get_tree().root.add_child(payment)
 	get_parent().get_parent().get_parent().queue_free()
