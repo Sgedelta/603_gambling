3f3a27483fe00b42f06a01a31b01397104702a6c
Gia Hur <129004059+giahur@users.noreply.github.com>
2026-04-06 20:44:59 -0400

    make doubloons under ads

--- Scenes/OutsideLink.gd
+++ Scenes/OutsideLink.gd
@@ -3,7 +3,6 @@ extends Button
 func _on_pressed() -> void:
 	var payment = load("res://Scenes/AD_Microtransaction.tscn").instantiate()
 	payment.process_mode = Node.PROCESS_MODE_ALWAYS
-	payment.z_index = 100
 	payment.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
 	get_tree().root.add_child(payment)
 	get_parent().get_parent().get_parent().queue_free()
