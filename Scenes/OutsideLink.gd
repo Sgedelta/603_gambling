9a0d1f7b2d8b84de3a8dff569072bfb3e5ec1020
Gia Hur <129004059+giahur@users.noreply.github.com>
2026-04-16 16:46:19 -0400

    critically acclaimed mmorpg

--- Scenes/OutsideLink.gd
+++ Scenes/OutsideLink.gd
@@ -1,4 +1,9 @@
 extends Button
 
+@onready var canvasLayer = get_parent().get_parent().get_parent()
+
 func _on_pressed() -> void:
-	OS.shell_open("https://www.ncpgambling.org/help-treatment/#:~:text=Gambling%20Helpline%E2%84%A2-,The%20National%20Problem%20Gambling%20Helpline%E2%84%A2%20(1%2D800%2DMY,50%20states%20and%20U.S.%20territories.")
+	if(canvasLayer.vidnum == 0):
+		OS.shell_open("https://na.finalfantasyxiv.com/")
+	else:
+		OS.shell_open("https://www.ncpgambling.org/help-treatment/#:~:text=Gambling%20Helpline%E2%84%A2-,The%20National%20Problem%20Gambling%20Helpline%E2%84%A2%20(1%2D800%2DMY,50%20states%20and%20U.S.%20territories.")
