d8945d41f18f75144826b890100d246131ea810b
Samuel Easton (RIT Student) <sge3418@rit.edu>
2026-04-16 16:46:44 -0400

    I added three variables :>

--- Scenes/Drinks.cs
+++ Scenes/Drinks.cs
@@ -3,6 +3,10 @@ using System;
 
 public partial class Drinks : StaticBody2D
 {
+	[Export] public float DrinkCost = 5;
+	[Export] public float DrinkHopeStr = .1f;
+	[Export] public float DrinkAddictionStr = .025f;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,4 +16,11 @@ public partial class Drinks : StaticBody2D
 	public override void _Process(double delta)
 	{
 	}
+
+
+	public void BuyDrink(Customer c)
+	{
+
+	}
+
 }
