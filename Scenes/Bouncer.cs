ddc10090efb5002576da2b8042c66efe724e1fed
rmt8943 <78225251+rmt8943@users.noreply.github.com>
2026-05-04 21:17:53 -0400

    working

--- Scenes/Bouncer.cs
+++ Scenes/Bouncer.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,19 +8,19 @@ using System.Diagnostics;
 public partial class Bouncer : Node2D
 {
 	private Queue<int> bouncerCost = new Queue<int>(new[] {500, 100, 200, 350, 500, 750});
-	public Texture[] sprites = new Texture[6];
+	public Array<Texture2D> sprites = new Array<Texture2D>();
 	int index = 1;
 	public float StopTime = 1.0f;
 	public bool IsHired = false;
 
 	void Start()
 	{
-		sprites[0] = GD.Load<Texture>("res://Resources/Sprites/bouncer.png");
-		sprites[1] = GD.Load<Texture>("res://Resources/Sprites/bouncer2.png");
-        sprites[2] = GD.Load<Texture>("res://Resources/Sprites/bouncer3.png");
-        sprites[3] = GD.Load<Texture>("res://Resources/Sprites/bouncer4.png");
-        sprites[4] = GD.Load<Texture>("res://Resources/Sprites/bouncer5.png");
-        sprites[5] = GD.Load<Texture>("res://Resources/Sprites/bouncer6.png");
+		sprites[0] = ResourceLoader.Load<Texture2D>("res://Resources/Sprites/bouncer.png");
+		sprites[1] = ResourceLoader.Load<Texture2D>("res://Resources/Sprites/bouncer2.png");
+        sprites[2] = ResourceLoader.Load<Texture2D>("res://Resources/Sprites/bouncer3.png");
+        sprites[3] = ResourceLoader.Load<Texture2D>("res://Resources/Sprites/bouncer4.png");
+        sprites[4] = ResourceLoader.Load<Texture2D>("res://Resources/Sprites/bouncer5.png");
+        sprites[5] = ResourceLoader.Load<Texture2D>("res://Resources/Sprites/bouncer6.png");
     }
 	
 	private void IncreaseStopTime()
@@ -59,7 +60,7 @@ public partial class Bouncer : Node2D
 			}
 			else
 			{
-				GetNode<Sprite2D>("/root/MainGame/Bouncer/Display/Sprite").Texture = (Texture2D)sprites[index];
+				GetNode<Sprite2D>("Display/Sprite").Texture = sprites[index];
 				index++;
 				IncreaseStopTime();
 			}
